363686b497cf311f6c7fe9337dad4417e44dba83
Sam Larsen-Disney <sam@amplitude.com>
2022-07-14 21:57:25 +0100

    lint

--- components/booking/form/Deposit.js
+++ components/booking/form/Deposit.js
@@ -68,7 +68,7 @@ const Deposit = () => {
           <div className="px-4 sm:px-0">
             <h3 className="text-lg font-medium leading-6">Pay your deposit.</h3>
             <p className="mt-1 text-sm text-gray-200">
-              It's now time to pay your deposit. Deposit for your{" "}
+              It&apos;s now time to pay your deposit. Deposit for your{" "}
               {store.productName} is {store.depositPerc * 100}% of the total
               price.
             </p>
